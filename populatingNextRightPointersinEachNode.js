4b1b160fbf8cf6a72a5f17459e9dec98bba5d6ca
gdomorski <gregdomorski@gmail.com>
2016-02-01 10:54:51 -0500

    Fix PopulatingNextRightPointersEachNode

--- populatingNextRightPointersinEachNode.js
+++ populatingNextRightPointersinEachNode.js
@@ -11,15 +11,50 @@
  * @return {void} Do not return anything, modify tree in-place instead.
  */
 
+/*
+
+var root = new TreeLinkNode(1);
+var two = new TreeLinkNode(2);
+var three = new TreeLinkNode(3);
+var four = new TreeLinkNode(4);
+var five = new TreeLinkNode(5);
+var six = new TreeLinkNode(6);
+var seven = new TreeLinkNode(7);
+
+root.left = two;
+root.right = three;
+root.left.left = four;
+root.left.right = five;
+root.right.left = six;
+root.right.right = seven;
+
+*/
+
 var connect = function(root) {
-	root.next = null;
 
-	//loop down the tree
-	//link the left pointers to the right pointers, forming a linked list
-	//have the one at the end point to null
-	//call it recursively.
+var obj = {};
 
-	//return the root
+function traverse (rooty, level) {
+    if(rooty === null){
+        return;
+    }
+    if(!obj[level]) {
+        obj[level] = [rooty];
+    }else{
+        obj[level].push(rooty);
+    }
+    traverse(rooty.left, level + 1);
+    traverse(rooty.right, level + 1);
+}
 
+traverse(root, 0);
+
+for(var key in obj){
+    for(var i = 0; i < obj[key].length - 1; i++){
+     obj[key][i].next = obj[key][i + 1];
+    }
+}
+
+return root;
 
 };
